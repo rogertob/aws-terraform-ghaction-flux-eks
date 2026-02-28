658f520beed1a76b62341b0645f160e5edc916d9
Rodny G. <159080967+rogertob@users.noreply.github.com>
2026-02-27 20:55:17 -0500

    providers

--- terraform/modules/flux/main.tf
+++ terraform/modules/flux/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    flux = {
+      source = "fluxcd/flux"
+    }
+    github = {
+      source = "integrations/github"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
 resource "flux_bootstrap_git" "this" {
   path    = var.flux_target_path
   version = var.flux_version != "" ? var.flux_version : null
