ca26e05cc62b954c18ae14257ad6dd92b7677646
Rodny G. <159080967+rogertob@users.noreply.github.com>
2026-02-28 00:16:23 -0500

    config mgmt

--- terraform/modules/flux/main.tf
+++ terraform/modules/flux/main.tf
@@ -22,4 +22,29 @@ resource "flux_bootstrap_git" "this" {
     "image-reflector-controller",
     "image-automation-controller",
   ]
+
+  kustomization_override = <<-EOT
+    apiVersion: kustomize.config.k8s.io/v1beta1
+    kind: Kustomization
+    resources:
+    - gotk-components.yaml
+    - gotk-sync.yaml
+    patches:
+      - patch: |
+          apiVersion: apps/v1
+          kind: Deployment
+          metadata:
+            name: all
+          spec:
+            template:
+              spec:
+                tolerations:
+                  - key: CriticalAddonsOnly
+                    operator: Exists
+                nodeSelector:
+                  node-role: system
+        target:
+          kind: Deployment
+          namespace: flux-system
+    EOT
 }
